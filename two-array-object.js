8398fb7b87ed66b444b795884bc56f1760db6943
William Valadez <billy.valadez@icloud.com>
2023-07-26 18:34:23 -0400

    finished two-array-object with O(n) time complexity

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,38 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+
+// ensure the function takes in two arrays
+// outputs an object based on those arrays
+// first array being keys and second being values
+// if not enough values sey key value to null
+// if not enough key ignore remaining values
+
+// exmaples
+
+// easy example: [will, chloe, sam, nilo] [1,2,3,4] output: {will: 1, chloe: 2, sam: 3, nilo: 4}
+
+// another example: [a,b,c] [1,2] output: { a: 1, b: 2, c: null}
+
+// another example: [a,b,c] [1,2,3,4] { a: 1, b: 2, c: 3}
+
+// initilize object
+
+// loop over first arr
+// if obj is empty
+// then place key and place 2nd arr val
+// if 2nd arr currval === undefined then replace null
+
+// return object
+
+function twoArrayObject(keys, vals) {
+  let obj = {};
+
+  for (let i = 0; i < keys.length; i++) obj[keys[i]] = vals[i] || null;
+
+  return obj;
 }
+
+twoArrayObject(["a", "b", "c", "d"], [1, 2, 3]); // {'a': 1, 'b': 2, 'c': 3, 'd': null}
+twoArrayObject(["a", "b", "c"], [1, 2, 3, 4]); // {'a': 1, 'b': 2, 'c': 3}
+twoArrayObject(["x", "y", "z"], [1, 2]); // {'x': 1, 'y': 2, 'z': null}
+
+module.exports = { twoArrayObject };
--- two-array-object.test.js
+++ two-array-object.test.js
@@ -1,26 +1,28 @@
-describe("twoArrayObject", function() {
-  it("makes an object of keys and values", function() {
+const { twoArrayObject } = require("./two-array-object");
+
+describe("twoArrayObject", function () {
+  it("makes an object of keys and values", function () {
     expect(twoArrayObject(["a", "b", "c"], [1, 2, 3])).toEqual({
       a: 1,
       b: 2,
-      c: 3
+      c: 3,
     });
   });
 
-  it("ignores extra values", function() {
+  it("ignores extra values", function () {
     expect(twoArrayObject(["a", "b", "c"], [1, 2, 3, 4])).toEqual({
       a: 1,
       b: 2,
-      c: 3
+      c: 3,
     });
   });
 
-  it("sets values for extra keys to null", function() {
+  it("sets values for extra keys to null", function () {
     expect(twoArrayObject(["a", "b", "c", "d"], [1, 2, 3])).toEqual({
       a: 1,
       b: 2,
       c: 3,
-      d: null
+      d: null,
     });
   });
 });
